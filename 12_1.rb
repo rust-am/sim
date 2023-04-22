fc4aeef3de6f52c3d7fc9de1a0fe401d19f58380
Rustam <r.91@mail.ru>
2023-04-22 09:47:39 +0300

    refactor

--- 12_1.rb
+++ 12_1.rb
@@ -1,7 +1,13 @@
 class Array
   def my_map
     new_arr = []
-    self.each { |e| new_arr << yield(e) } if block_given?
+
+    if block_given?
+      self.each { |e| new_arr << yield(e) }
+    else
+      out = to_enum :mapp
+    end
+    
     new_arr
   end
 end
