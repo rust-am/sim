96ca6c1bb22f06d015c33ffb36041ebe9383663b
Rustam <r.91@mail.ru>
2023-04-25 23:36:09 +0300

    chapter 12 4th task

--- 12_4.rb
+++ 12_4.rb
@@ -1,3 +1,12 @@
 arr = [[[1, 2], 3], [4, 5, 6], [7, [8, 9]]]
 
+def walk(el, &block)
+  case el
+  when Array
+    el.each { |e| walk(e, &block) }
+  when Integer
+    block.call(el)
+  end
+end
+
 walk(arr) { |i| puts i }
